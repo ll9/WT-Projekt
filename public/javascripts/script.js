81f80fc823ceaeb234f4a042eb70ed9b42daff76
ll9 <llwierer@web.de>
2017-12-07 18:57:15 +0100

    moved genres to vue

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,29 @@
 new Vue({
     el: '#select',
     data: {
-        selected: ''
+        selected: '',
+        genres: [
+            'Action',
+            'Adventure',
+            'Animation',
+            'Biography',
+            'Comedy',
+            'Crime',
+            'Documentary',
+            'Drama',
+            'Family',
+            'Fantasy',
+            'History',
+            'Horror',
+            'Music',
+            'Mystery',
+            'Romance',
+            'Sci-Fi',
+            'Short',
+            'Sport',
+            'Thriller',
+            'War',
+            'Western'
+        ]
     }
-})
+});
