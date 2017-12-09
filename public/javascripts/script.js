7ddc60467d6f06f5108642078d35ed0671ae763a
ll9 <llwierer@web.de>
2017-12-09 12:12:40 +0100

    vue implemented rating, actors, title, genres, overview

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 
-function Movie(title, genre, description, rating, image, trailer, actors) {
+function Movie(title, genres, description, rating, image, trailer, actors) {
     this.getTitle = () => {return title;}
-    this.getGenre = () => {return genre;}
+    this.getGenres = () => {return genres;}
     this.getDescription = () => {return description;}
     this.getRating = () => {return rating;}
     this.getImage = () => {return image;}
@@ -36,12 +36,23 @@ new Vue({
             'Thriller',
             'War',
             'Western'
-        ]
+        ],
+        currentMovies: []
     },
     methods: {
         searchMovies: function() {
             this.$http.get('/search/' + this.$data.searchText).then(resp => {
-                console.log(resp.body);
+                this.$data.currentMovies = [];
+                
+                for (movie of resp.body) {
+                    this.$data.currentMovies.push(
+                        //new Movie(movie.title, movie.genres.map(obj => obj.name).join(", "), movie.overview,
+                        //    movie.vote_average, movie.poster_path, movie.videos, movie.credits)
+                        new Movie(movie.title, movie.genres.map(obj => obj.name).join(", "), movie.overview,
+                            movie.vote_average, null, null, movie.credits.cast.slice(0, 4).map(obj => obj.name).join(", "))
+                    );
+                }
+                console.log(this.$data.currentMovies);
             });
         }
     }
