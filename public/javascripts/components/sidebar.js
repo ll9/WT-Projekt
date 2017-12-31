adc41027cabd4102279ee69bc540c2c7ebf94108
ll9 <llwierer@web.de>
2017-12-31 23:18:24 +0100

    vue router
    cleaned router

--- public/javascripts/components/sidebar.js
+++ public/javascripts/components/sidebar.js
@@ -5,22 +5,23 @@ Vue.component('sidebar', {
             <div>
                 <h1 class="title" style="text-align:center">MyML</h1>
             </div>
-            <a href="/" style="color: black;">
+            <router-link to="/">
                 <div class="icon1">
                     <i class="fa fa-home" aria-hidden="true"></i>
                 </div>
-            </a>
+            </router-link>
             <br>
-            <a @click="redirectIfloggedIn('/watching')" style="color: black; cursor: pointer;">
+            <router-link to="watching">
                 <div class="icon2">
                     <i class="fa fa-eye" aria-hidden="true"></i>
                 </div>
-            </a>
+            </router-link>
             <br>
-            <a @click="redirectIfloggedIn('/watched')" style="color: black; cursor: pointer;">
+            <router-link to="/watched">
                 <div class="icon3">
                     <i class="fa fa-check-square-o" aria-hidden="true"></i>
                 </div>
+            </router-link>
             </a>
             <login v-bind:is-logged-in="isLoggedIn" v-on:success="isLoggedIn=!isLoggedIn"></login>
         </div>
--- public/javascripts/watching.js
+++ public/javascripts/watching.js
@@ -1,20 +1,9 @@
  const Watching = Vue.component('Watching', {
     template: `
     <div>
-        <sidebar></sidebar>
-        <h1 style="text-align:center">Watched Page</h1>
-        <p style="text-align:center">
-            Insert movies the user put into his list here. Suggestion for Pseudo Code:
-        </p>
-        <p style="text-align:center">
-            &lt;template v-for="movie of movies"&gt;
-            <br>
-            &lt;watchlist v-bind:movie="movie"&gt;
-            &lt;/watchlist&gt;
-            <br>
-            &lt;/template&gt;
-        </p>
-        <p style="text-align:center"> The Watchlist tags still needs to be created as a Vue Tag (see movieField.js)
+        <sidebar is-logged-in="true"></sidebar>
+        <h2 style="text-align:center">Watchinglist</h2>
+        <watchlist :movies="movies"></watchlist>
     </div>
     `,
     data: function() {
