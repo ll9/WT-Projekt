ec29912ed196ec0ae852fee962bd30e99bc39b3c
ll9 <llwierer@web.de>
2018-01-12 16:56:27 +0100

    api.js refactoring
    more api.js refactoring
    
    more api.js refactoring

--- routes/api.js
+++ routes/api.js
@@ -2,129 +2,87 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+const watching = "watching";
+const watched = "watched";
 
-router.get('/user/watching', (req, res) => {
-    req.userCollection.findOne({
-        _id: req.user
-    }).then(user => {
-        // create array which contains only the ids (without rating)
-        var movielist = user.watching.map(watching => watching.movie_id);
-        req.movieCollection.find({
-            id: {
-                $in: movielist
-            }
-        }).then(movies => {
-            res.json(movies).status(200).end();
+
+function checkAuth(req, res, next) {
+    if (!req.user) {
+        res.status(401).end()
+    } else {
+        next();
+    }
+}
+
+function getMoviesFromList(list) {
+    return (req, res) => {
+        req.userCollection.findOne({
+            _id: req.user
+        }).then(user => {
+            req.movieCollection.find({
+                id: {
+                    $in: user[list].map(entry => entry.movie_id) // create array which contains only the ids (without rating)
+                }
+            }).then(movies => res.json(movies).status(200).end());
         })
-    })
-})
+    }
+}
 
-router.get('/user/watched', (req, res) => {
-    req.userCollection.findOne({
-        _id: req.user
-    }).then(user => {
-        // create array which contains only the ids (without rating)
-        var movielist = user.watched.map(watched => watched.movie_id);
-        req.movieCollection.find({
-            id: {
-                $in: movielist
-            }
-        }).then(movies => {
-            res.json(movies).status(200).end();
-        })
-    })
-})
-
-//Add to Watchlist
-router.post('/watchlist/add', (req, res) => {
-    if (req.user) {
+function addMovieToList(list) {
+    return (req, res) => {
         req.userCollection.findOne({
             _id: req.user
         }).then(user => {
             // check if movie is not already in watchlist
-            if (!(user.watching.map(watching => watching.movie_id).includes(req.body.movie_id))) {
+            if (!(user[list].map(entry => entry.movie_id).includes(req.body.movie_id))) {
                 req.userCollection.update({
                     _id: req.user
                 }, {
                     $push: {
-                        watching: {
+                        [list]: {
                             movie_id: req.body.movie_id,
                             rating: null
                         }
                     }
-                })
+                }).then(() => res.status(201).end())
             }
         })
-        res.end();
     }
-})
+}
 
-router.post('/watchedlist/add', (req, res) => {
-    if (req.user) {
-        req.userCollection.findOne({
-            _id: req.user
-        }).then(user => {
-            // check if movie is not already in watchlist
-            if (!(user.watched.map(watched => watched.movie_id).includes(req.body.movie_id))) {
-                req.userCollection.update({
-                    _id: req.user
-                }, {
-                    $push: {
-                        watched: {
-                            movie_id: req.body.movie_id,
-                            rating: null
-                        }
-                    }
-                })
-            }
-        })
-        res.end();
-    }
-})
-
-router.delete('/watchlist/remove', (req, res) => {
-    if (req.user) {
+function removeMovieFromList(list) {
+    return (req, res) => {
         req.userCollection.findOne({
             _id: req.user
         }).then(user => {
             // check if movie is already in watchlist
-            if ((user.watching.map(watching => watching.movie_id).includes(req.body.movie_id))) {
+            if ((user[list].map(watching => watching.movie_id).includes(req.body.movie_id))) {
                 req.userCollection.update({
                     _id: req.user
                 }, {
                     $pull: {
-                        watching: {
+                        [list]: {
                             movie_id: req.body.movie_id
                         }
                     }
-                })
+                }).then(() => res.status(204).end())
             }
         })
-        res.end();
     }
-})
+}
 
-router.delete('/watchedlist/remove', (req, res) => {
-    if (req.user) {
-        req.userCollection.findOne({
-            _id: req.user
-        }).then(user => {
-            // check if movie is already in watchlist
-            if ((user.watched.map(watched => watched.movie_id).includes(req.body.movie_id))) {
-                req.userCollection.update({
-                    _id: req.user
-                }, {
-                    $pull: {
-                        watched: {
-                            movie_id: req.body.movie_id,
-                        }
-                    }
-                })
-            }
-        })
-        res.end();
-    }
-})
+
+router.get('/user/watching', getMoviesFromList(watching));
+
+router.get('/user/watched', getMoviesFromList(watched));
+
+router.post('/watchlist/add', checkAuth, addMovieToList(watching));
+
+router.post('/watchedlist/add', checkAuth, addMovieToList(watched));
+
+router.delete('/watchlist/remove', checkAuth, removeMovieFromList(watching));
+
+router.delete('/watchedlist/remove', checkAuth, removeMovieFromList(watched));
 
 
 router.get('/search/movies/:movieName', (req, res, next) => {
