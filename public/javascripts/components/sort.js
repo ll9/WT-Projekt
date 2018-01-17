b8565f9aac8d2c613ad0159a9f1789adb2c3cd03
ll9 <llwierer@web.de>
2018-01-17 07:38:44 +0100

    sort.js bootstrsap select bugfix

--- public/javascripts/components/sort.js
+++ public/javascripts/components/sort.js
@@ -1,11 +1,20 @@
 const test = Vue.component('sort', {
     template: `
 <div class="sortbox">
-    <select class="selectpicker" v-model="orderBy" data-style="btn-primary" data-width="fit">
+    <select class="selectpicker" ref="sortselect" v-model="orderBy" data-style="btn-primary" data-width="fit">
       <option>Popularity</option>
       <option>Title</option>
       <option>Rating</option>
     </select>
 </div>
   `,
+    mounted: function() {
+        // make sure that bootstrap-select behaves/reloads properly
+        $(this.$refs.sortselect).selectpicker('refresh');
+    },
+    data: function() {
+        return {
+            orderBy: null
+        }
+    }
 });
