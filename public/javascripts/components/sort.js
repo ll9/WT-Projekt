07ed95c778d5171bd3f0e947249cb73e617b87aa
ll9 <llwierer@web.de>
2018-01-17 12:43:59 +0100

    basic layout for sort

--- public/javascripts/components/sort.js
+++ public/javascripts/components/sort.js
@@ -1,11 +1,13 @@
 const sort = Vue.component('sort', {
     template: `
 <div class="sortbox">
+    <span style="color: black;">Order By </span>
     <select class="selectpicker" ref="sortselect" v-model="orderBy" data-style="btn-primary" data-width="fit">
       <option>Popularity</option>
       <option>Title</option>
       <option>Rating</option>
     </select>
+    <span><i class="fa fa-arrow-down" aria-hidden="true"></i></span>
 </div>
   `,
     mounted: function() {
