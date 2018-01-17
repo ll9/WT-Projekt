119d7d3970a9936e086d43a9176e62a40fde8203
BacherM <michael-bacher@hotmail.de>
2018-01-17 11:29:04 +0100

    Updated Watchlist

--- public/javascripts/components/watchlist.js
+++ public/javascripts/components/watchlist.js
@@ -2,30 +2,27 @@ Vue.component('watchlist', {
     props: ['movie'],
     template: `
 <div>
-    <div class="main" style="position:relative;">
+    <div class="wl_main" style="position:relative;">
         <div style="text-align:center;" class="ov_rating">
             <p> {{movie.getRating() }} </p>
         </div>
         <div class="t_main" style="text-align:center;">
             <a target="_blank" :href="movie.getTitleLink()" style="color: white; text-decoration: none;"> {{movie.getTitleYear()}} </a>
         </div>
+        <div class="wl_watched">"Watched" button einfügen</div>
+        <div class="wl_remove">"Remove from WL" Button einfügen</div>
         <div class="g_main" style="text-align:center;">
             <p> {{movie.getGenres() }} </p>
         </div>
-        <div class="d_main" style="text-align:center;">
-            <p> {{movie.getDescription()}} </p>
-        </div>
-        <div class="a_main" style="text-align:center;">
+        <div class="wl_actor" style="text-align:center;">
             <template v-for="(actor,index) of movie.getActors()">
             <a target="_blank" v-bind:href="movie.getActorLink(actor)">
                 {{actor.name}}</a><span v-if="(index+1)!==movie.getActors().length">, </span>
                 </template>
             </div>
+            <div class="pr">PR hier einfügen</div>
             <div class="img" style="text-align:center;">
                 <img v-bind:src="movie.getImage()" alt="movie image">
-            </div>
-            <div class="trailer" style="text-align:center;">
-                <a v-if="movie.getTrailer()" v-bind:href="movie.getTrailer()" target="_blank">Watch Trailer</a>
             </div>
         </div>
         <div class="add">
