4f156c3f2441e0124ea4586cf83f8127972a8ced
ll9 <llwierer@web.de>
2018-01-18 08:34:45 +0100

    nasty bugfix (rating wouldn't update correctly when movie got deleted)

--- public/javascripts/watched.js
+++ public/javascripts/watched.js
@@ -2,7 +2,7 @@ const Watched = Vue.component('Watched', {
     template: `
     <div>
         <h2 style="text-align:center">Watchedlist</h2>
-        <watchlist v-for="(movie, index) of movies" :movie="movie" :key="index" 
+        <watchlist v-for="movie of movies" :movie="movie" :key="movie.getId()" 
         v-on:rating-request="saveRating"
         v-on:delete-movie="deleteMovie">
         </watchlist>
--- public/javascripts/watching.js
+++ public/javascripts/watching.js
@@ -2,7 +2,7 @@ const Watching = Vue.component('Watching', {
     template: `
     <div>
         <h2 style="text-align:center">Watchinglist</h2>
-        <watchlist v-for="(movie, index) of movies" :movie="movie" :key="index" 
+        <watchlist v-for="movie of movies" :movie="movie" :key="movie.getId()" 
         v-on:rating-request="saveRating"
         v-on:delete-movie="deleteMovie">
         </watchlist>
