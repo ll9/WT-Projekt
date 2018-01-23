e9f48027e7a61287e4cce749bf43d3ce47c5f35d
ll9 <llwierer@web.de>
2018-01-23 09:30:39 +0100

    cleaning

--- public/javascripts/classes/Movie.js
+++ public/javascripts/classes/Movie.js
@@ -1,36 +1,36 @@
-function Movie (movie) {   
+function Movie(movie) {
+    this.getId = () => movie.id;
     this.getYear = () => movie.release_date.split('-')[0];
     this.getTitle = () => movie.title;
     this.getTitleYear = () => `${this.getTitle()} (${this.getYear()})`;
     this.getTitleLink = () => 'https://www.themoviedb.org/movie/' + movie.id;
+    this.getPopularity = () => movie.popularity
+    this.getGenres = () => movie.genres.map(obj => obj.name).join(", ");
+    this.getDescription = () => movie.overview;
+    this.getRating = () => this.isReleased() ? movie.vote_average : 'tbd';
     this.getDate = () => new Date(movie.release_date);
     this.isReleased = () => {
         let today = new Date();
         let release = new Date(movie.release_date)
         return today > release;
     }
-    this.getPopularity = () => movie.popularity
-    this.getGenres = () => movie.genres.map(obj => obj.name).join(", ");
-    this.getDescription = () => movie.overview;
-    this.getRating = () => this.isReleased() ? movie.vote_average : 'tbd';
     this.getImage = () => {
         const baseURL = "https://image.tmdb.org/t/p/";
         const picutreSize = "w150/";
         if (movie.poster_path == null)
             return "http://via.placeholder.com/150x240";
         return baseURL + picutreSize + movie.poster_path;
-    };
+    }
     this.getTrailer = () => {
         const baseURL = "https://www.youtube.com/watch?v=";
         if (movie.videos.results.length === 0)
             return null;
         else
-            return baseURL + movie.videos.results[0].key
-    };
+            return baseURL + movie.videos.results[0].key;
+    }
     this.getActors = () => movie.credits.cast.slice(0, 4);
     this.getActorLink = (actor) => {
         const baseURL = "https://www.themoviedb.org/person/";
         return baseURL + actor.id + '-' + actor.name.replace(/ /g, '-');
     }
-    this.getId = () => movie.id;
 }
